db6e1c100e8689cbd0c5a20a95d082fd87aa6435
Stevi28 <stav.kourou19@gmail.com>
2026-03-26 03:03:06 +0200

    fix containerd script

--- scripts/setup-containerd.sh
+++ scripts/setup-containerd.sh
@@ -4,14 +4,22 @@
 
 echo "Starting standalone containerd installation..."
 
+# detect architecture
+ARCH=$(uname -m)
+if [ "$ARCH" = "aarch64" ]; then
+  ARCH="arm64"
+else
+  ARCH="amd64"
+fi
+
 # download the containerd binary 
-wget https://github.com/containerd/containerd/releases/download/v2.0.0/containerd-2.0.0-linux-amd64.tar.gz
+wget https://github.com/containerd/containerd/releases/download/v2.0.0/containerd-2.0.0-linux-${ARCH}.tar.gz
 
 # extract the binaries to /usr/local
-sudo tar -C /usr/local -xzvf containerd-2.0.0-linux-amd64.tar.gz
+sudo tar -C /usr/local -xzvf containerd-2.0.0-linux-${ARCH}.tar.gz
 
 # clean up the archive
-rm containerd-2.0.0-linux-amd64.tar.gz
+rm containerd-2.0.0-linux-${ARCH}.tar.gz
 
 # verify the installation using the 'ctr' tool
 echo "Verifying installation with 'ctr'..."
